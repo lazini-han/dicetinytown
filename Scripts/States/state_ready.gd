2b2b9fc31f823be8d36ecafadd0c337eceb54f92
Jinhee Han <zinnahan0@gmail.com>
2025-03-20 01:49:35 +0900

    restart

--- Scripts/States/state_ready.gd
+++ Scripts/States/state_ready.gd
@@ -8,11 +8,9 @@ func get_state_enum():
 
 func enter():
 	print("상태 진입: READY")
-	Eventbus.emit_signal("ButtonRollDice_change",true) # 주사위 굴림 활성화
-	Eventbus.emit_signal("ButtonUndo_change",false) # Undo 버튼 비활성화
+
 	
 
 func exit():
 	print("상태 종료: READY")
-	Eventbus.emit_signal("ButtonRollDice_change",false) # 주사위 굴림 버튼 비활성화
 	
